c3dfbd5e9d913c8c993281b3392f9699d31d882d
Gino Valentino Pensuni <85497@fhwn.ac.at>
2015-06-16 11:29:31 +0200

    Added Class Library

---
+++ ProjektwocheTeam1/GuiRenderer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektwocheTeam1
+{
+    class GuiRenderer
+    {
+    }
+}
